013f5c309ad7b45dd33599336b137c0f82fb36eb
Sr-Pedro <141073848+Sr-Pedro@users.noreply.github.com>
2023-07-31 17:27:47 -0300

    Update calculo.py
    Enviando um código mais curto e quero ver se ele é aprovável

--- Nível 2/2021/Fase 2/calculo.py
+++ Nível 2/2021/Fase 2/calculo.py
@@ -1,3 +1,5 @@
+#Deixei as mudanças no final do arquivo
+
 S = int(input())
 
 A = int(input())
@@ -31,4 +33,32 @@ for i in range(len(ListaIntervalo)):
 print(len(list(set((ListaNumerosNoIntervaloIguaisAS)))))
 #print(len(ListaNumerosNoIntervaloIguaisAS))
 
+
+S = input()
+A = input()
+B = input()
+
+#Variável que contará o número de valores cuja soma dos dígitos é igual a S
+Count = 0
+
+#Usando range para percorrer os elementos de A a B
+for x in range(int(A),int(B)+1):
     
+    #Estabelecendo uma variável que será usada posteriormente
+    
+    sum = 0
+
+    #Iterando sobre cada elemento do intervalo inteiro de A a B.
+    #Para que a iteração ocorra, converte-se o valor em string
+    #Essa parte do código visa pegar cada dígito e depois convertê-lo para int
+    #Convertendo-o, somaremos este à variável anteriormente estabelecida e assim poderemos observar a soma dos dígitos do valor
+
+    for y in str(x):
+        sum += int(y)
+    #Condicional para ver se a soma dos dígitos do valor é igual a S
+    if sum == int(Soma):
+        Count += 1
+    else:
+        continue
+
+print(C)
